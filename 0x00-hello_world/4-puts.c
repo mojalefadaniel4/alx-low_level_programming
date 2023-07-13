c2cc35ec2ac09d560350425967b69626d68bf65b
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-13 14:43:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 ing program that prints a line with puts functio
 n
 * Return: o (success)
-*\
+**/
+
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzl
-e");
+	puts ("Programming is like building a multilingual puzzle");
 return (0);
 }
