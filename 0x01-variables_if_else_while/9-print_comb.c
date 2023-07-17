88b300bb6b81b750dcd8f3608398217b285fbe72
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-17 14:06:57 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
+	int  i = "0";
+	while (i <= "9"
+	{
+			putchar(i);
+			if (i != "9")
+                        {
+			putchar("'");
+			putchar("");
+			}
+        ++i;
+	}
+	putchar("\n");
 	return (0);
 }
