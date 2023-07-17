7833578d6faa32ce601e134df6ad746fc491af5a
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-17 13:52:11 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -7,5 +8,13 @@
  */
 int main(void)
 {
+	int num;
+	char letter;
+
+	for (num  = 0; num < 10; num++)
+		putchar((num % 10) + "0");
+	for letter = "a"; letter <= "f"; letter++)
+		putchar(letter);
+	putchar("\n");
 	return (0);
 }
