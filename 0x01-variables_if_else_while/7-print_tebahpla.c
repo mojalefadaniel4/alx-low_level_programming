16adb93e6a6cfd53a6da704283a9af0db2264f27
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-17 13:33:16 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -8,9 +7,5 @@
  */
 int main(void)
 {
-	int num;
-	fcr (num = 0; num < 10; num++)
-		putchar((num % 10) + "0");
-	putchar("\n");
 	return (0);
 }
