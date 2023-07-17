1c35a297dca7ae27f13814aa52988a110bb7ea54
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-17 09:35:10 -0700

    q1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+
+#Get file name
+echo "0-positive_or_negative.c
+read fname
+
+#Create the file if the file does not exist
+if [ -f "$fname" ]; then
+
+#Execute file
+chmod u+x $fname
+
+#Push File Up stream
+git add .
+git commit -m "$fname"
+git push
+else
+
+echo -e  "#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}" > $fname
+
+#Execute file
+chmod u+x $fname
+
+#Push File Up stream
+git add .
+git commit -m "$fname"
+git push
+
+
