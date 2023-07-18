bc9b7026b3133e1d4bd344fbe0dcbcfb4d6418e0
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-18 11:55:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
-#include 0-putchar.c
+#include "main.h"
+
 /**
- *  main - entry point
- *  Return: Always 0
+ * main - Entry point
+ *Description : 'print out _putchar to output'
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r);
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+
 }
