683becf7fb4082c3d8256217e305f3e465397fb7
mojalefadniel4 <mojalefadaniel2221@gmail.com>
2023-07-24 13:15:03 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,15 +1,25 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts_half - a take any string and print half of it
+ * at every odd number lenghth, n = (length_of_the_string - 1) / 2
+ * @strn: input
+ * Return: half of whatever the input was
  */
-int main(void)
+void puts_half(char *strn)
 {
-    char *str;
+	int i, j;
 
-    str = "0123456789";
-    puts_half(str);
-    return (0);
+	int length = 0;
+
+	for (i = 0; strn[i] != '\0'; i++)
+		length++;
+
+	j = (length / 2);
+
+	if ((length % 2) == 1)
+		j = ((length + 1) / 2);
+
+	for (i = j; strn[i] != '\0'; i++)
+		_putchar(strn[i]);
+	_putchar('\n');
 }
